a8be5d1b82ee77d717ff20a745b3aeffeaf93f25
吴厚祥 <1724189870@qq.com>
2022-07-20 14:26:49 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      formats: ['cjs', 'es']
+      formats: ['cjs', 'es'],
     },
     rollupOptions: {
       external: ['vue']
     }
   },
-  plugins: [vue()]
+  plugins: [vue(), dts({ insertTypesEntry: true, copyDtsFiles: false })]
 })

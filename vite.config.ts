4023f85c0612cd8757014fed623e8b5182c2b19a
吴厚祥 <1724189870@qq.com>
2022-07-21 17:39:28 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import postcssImport from "postcss-import"
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  //样式表插件
+  css:{
+    postcss:{
+      plugins:[
+        postcssImport,
+        autoprefixer,
+        // tailwindcss
+      ]
+    }
+  },
   build: {
     lib: {
       entry: 'src/index.ts',

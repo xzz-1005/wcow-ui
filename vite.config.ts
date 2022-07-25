fa90e7c4d3dadc903bb2c2a1836fe7275eafa5cb
吴厚祥 <1724189870@qq.com>
2022-07-25 17:56:16 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,39 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
+const componentsDir = resolve(__dirname, 'src')
+
 export default defineConfig({
   build: {
+    outDir: 'es',
     lib: {
-      entry: 'src/index.ts',
-      formats: ['cjs', 'es'],
+      entry: resolve(componentsDir, 'index.ts'),
+      name: 'WCowUI',
     },
     rollupOptions: {
+      output: [
+        {
+          format: 'cjs',
+          preserveModules: true,
+          preserveModulesRoot: componentsDir,
+          dir: 'lib',
+          entryFileNames: '[name].js'
+        },
+        {
+          format: 'es',
+          preserveModules: true,
+          preserveModulesRoot: componentsDir,
+          dir: 'es',
+          entryFileNames: '[name].mjs'
+        }
+      ],
       external: ['vue']
     }
   },
-  plugins: [vue(), dts({ insertTypesEntry: true, copyDtsFiles: false })]
+  plugins: [vue(), dts({
+    exclude: ['node_modules', 'playground', 'common/icons', 'common/mixins', 'common/utils'],
+    outputDir: ['lib', 'es'],
+    copyDtsFiles: false })]
 })

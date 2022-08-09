426e49f46e8cf42283e74c955f82e2be14aa4762
吴厚祥 <1724189870@qq.com>
2022-08-09 17:04:38 +0800

    fix 打包

--- vite.config.ts
+++ vite.config.ts
@@ -7,33 +7,41 @@ const componentsDir = resolve(__dirname, 'src')
 
 export default defineConfig({
   build: {
-    outDir: 'es',
+    // outDir: 'es',
     lib: {
       entry: resolve(componentsDir, 'index.ts'),
       name: 'WCowUI',
+      fileName: (format) => `wcow-ui.${format}.js`
     },
     rollupOptions: {
-      output: [
-        {
-          format: 'cjs',
-          preserveModules: true,
-          preserveModulesRoot: componentsDir,
-          dir: 'lib',
-          entryFileNames: '[name].js'
-        },
-        {
-          format: 'es',
-          preserveModules: true,
-          preserveModulesRoot: componentsDir,
-          dir: 'es',
-          entryFileNames: '[name].mjs'
+      output: {
+        // {
+        //   format: 'cjs',
+        //   preserveModules: true,
+        //   preserveModulesRoot: componentsDir,
+        //   dir: 'lib',
+        //   entryFileNames: '[name].js'
+        // },
+        // {
+        //   format: 'es',
+        //   preserveModules: true,
+        //   preserveModulesRoot: componentsDir,
+        //   dir: 'es',
+        //   entryFileNames: '[name].mjs'
+        // }
+        globals: {
+          vue: 'Vue'
         }
-      ],
+      },
       external: ['vue']
     }
   },
-  plugins: [vue(), dts({
-    exclude: ['node_modules', 'playground', 'common/icons', 'common/mixins', 'common/utils'],
-    outputDir: ['lib', 'es'],
-    copyDtsFiles: false })]
+  plugins: [
+    vue(),
+    dts({
+      exclude: ['node_modules', 'playground', 'common/icons', 'common/mixins', 'common/utils'],
+      outputDir: ['lib', 'es'],
+      copyDtsFiles: false
+    })
+  ]
 })
